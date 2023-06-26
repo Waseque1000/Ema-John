cfd0c8da9ea737441dc65bff219d2380bef7de20
Waseque Arafat <waseque.ndc@gmail.com>
2023-06-26 17:22:59 +0600

    shop added

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Product.css";
+
+const Product = (props) => {
+  const { name, img, seller, price, quantity, ratings } = props.product;
+  return (
+    <div className="product">
+      <img className="product" src={img} alt="" />
+
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price:$ {price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}</p>
+      </div>
+      <button className="btn-cart">Add to Cart </button>
+    </div>
+  );
+};
+
+export default Product;
